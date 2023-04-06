95f09e6325d90a8c7866ce85ae940b81826146ef
tom02-tj <129867740+tom02-tj@users.noreply.github.com>
2023-04-06 15:25:40 +0800

    Update TestController.java

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,2 +1,6 @@
-package com.example.controller;public class TestController {
+package com.example.controller;
+public class TestController {
+  public void main(String[] args){
+    System.out.println(11111); 
+  }
 }
